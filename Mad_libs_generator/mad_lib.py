125f62ca60410e3e1a6e38644291acecc0d5d2ca
Busiso Ndumo <busisondumo1@gmail.com>
2023-03-05 23:38:51 +0200

    mad_labs game

---
+++ Mad_libs_generator/mad_lib.py
@@ -0,0 +1,27 @@
+#Mad libs generator
+#Summer_trip
+###########################################################################################################################
+
+adj1 = input("Enter an adjective: ")
+adj2 = input("Enter an adjective: ")
+n1 = input("Enter a Noun: ")
+n2 = input("Enter a Noun: ")
+pn1 = input("Enter a plural noun: ")
+pn2 = input("Enter a plural noun: ")
+pn3 = input("Enter a plural noun: ")
+g1 = input("Enter a game: ")
+v1 = input("Enter a verb ending with 'ing': ")
+v2 = input("Enter a verb ending with 'ing': ")
+v3= input("Enter a verb ending with 'ing': ")
+
+##########################################################################################################################
+print("###################################################################################")
+print()
+print(f"A vacation is when you take a trip to some {adj1} place with your {adj2} family.")
+print(f"Usually you go to a place that is near a/an {n1} or up a/an {n2}.")
+print(f"A good vacation place is one where you can ride {pn1} or play {g1} or go hunting for {pn2}.")
+print(f"I like to spend my time {v1} or {v2}.")
+print(f"When parents go on a vacation, they spend their time eating three {pn3} a day, and fathers play golf and")
+print(f"mothers sit around {v3}.")
+print()
+print("###################################################################################")
